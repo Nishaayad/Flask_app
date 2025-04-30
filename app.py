dd59ccc0c3ba302f9d53642021db6d7845d4dfba
nikunj <nikunjyadav1516@gmail.com>
2025-04-30 16:18:38 +0530

    Sql_connection

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sqlite3 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -22,12 +23,29 @@ def contact():
 def form():
     return render_template('form.html')
 
-# Submit Page (for form)
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form['name']
     email = request.form['email']
+
+    # Database me insert karna
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (name, email) VALUES (?, ?)", (name, email))
+    conn.commit()
+    conn.close()
+
     return render_template('result.html', name=name, email=email)
+
+@app.route('/users')
+def users():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('users_data.html', users=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+
+# Table create karo (sirf 1 baar chalana h)
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL
+)
+''')
+
+conn.commit()
+conn.close()
+
+print("Users table created successfully!")
